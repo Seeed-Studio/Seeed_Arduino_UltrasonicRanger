35d8d9f5e0b32ce8f051f961e2a0f9703ac83f54
Baozhu Zuo <zuobaozhu@gmail.com>
2020-02-02 11:38:33 +0800

    Pretty printed the Arduino code with astyle

--- examples/UltrasonicDisplayOnTerm/UltrasonicDisplayOnTerm.ino
+++ examples/UltrasonicDisplayOnTerm/UltrasonicDisplayOnTerm.ino
@@ -1,33 +1,33 @@
 /*
- * UltrasonicDisplayOnTerm.ino
- * Example sketch for ultrasonic ranger
- *
- * Copyright (c) 2012 seeed technology inc.
- * Website    : www.seeed.cc
- * Author     : LG, FrankieChu
- * Create Time: Jan 17,2013
- * Change Log :
- *
- * The MIT License (MIT)
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
+    UltrasonicDisplayOnTerm.ino
+    Example sketch for ultrasonic ranger
+
+    Copyright (c) 2012 seeed technology inc.
+    Website    : www.seeed.cc
+    Author     : LG, FrankieChu
+    Create Time: Jan 17,2013
+    Change Log :
+
+    The MIT License (MIT)
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+*/
 
 
 /***************************************************************************/
@@ -41,23 +41,21 @@
 #include "Ultrasonic.h"
 
 Ultrasonic ultrasonic(7);
-void setup()
-{
-	Serial.begin(9600);
+void setup() {
+    Serial.begin(9600);
 }
-void loop()
-{
-	long RangeInInches;
-	long RangeInCentimeters;
-	
-	Serial.println("The distance to obstacles in front is: ");
-	RangeInInches = ultrasonic.MeasureInInches();
-	Serial.print(RangeInInches);//0~157 inches
-	Serial.println(" inch");
-	delay(250);
-	
-	RangeInCentimeters = ultrasonic.MeasureInCentimeters(); // two measurements should keep an interval
-	Serial.print(RangeInCentimeters);//0~400cm
-	Serial.println(" cm");
-	delay(250);
+void loop() {
+    long RangeInInches;
+    long RangeInCentimeters;
+
+    Serial.println("The distance to obstacles in front is: ");
+    RangeInInches = ultrasonic.MeasureInInches();
+    Serial.print(RangeInInches);//0~157 inches
+    Serial.println(" inch");
+    delay(250);
+
+    RangeInCentimeters = ultrasonic.MeasureInCentimeters(); // two measurements should keep an interval
+    Serial.print(RangeInCentimeters);//0~400cm
+    Serial.println(" cm");
+    delay(250);
 }
